6ff1c245f474f9ee07cda0c99b40addd3b08bb88
hoang1007 <20020414@vnu.edu.vn>
2021-08-20 11:39:07 +0700

    add gifs to talk command

--- src/main/java/com/gnaoh/command/real/TalkCommand.java
+++ src/main/java/com/gnaoh/command/real/TalkCommand.java
@@ -2,14 +2,22 @@ package com.gnaoh.command.real;
 
 import com.gnaoh.command.Command;
 import com.gnaoh.utilities.AnimeGif;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 
 public class TalkCommand extends Command {
     public void info() {
-        event.getChannel().sendMessage(AnimeGif.smile).queue();
+        MessageEmbed messageEmbed = new EmbedBuilder().setImage(AnimeGif.shine).build();
+        event.getChannel().sendMessage("Made by gnaoh").embed(messageEmbed).queue();
     }
 
     public void hello() {
         event.getChannel().sendMessage("hi " + event.getAuthor().getAsMention() + " ❤️").queue();
+    }
+
+    public void hi() {
+        MessageEmbed messageEmbed = new EmbedBuilder().setImage(AnimeGif.shy).build();
+        event.getChannel().sendMessage("hello " + event.getAuthor().getAsMention() + " ❤️").embed(messageEmbed).queue();
     }
 
     public void say(String message) {
--- src/main/java/com/gnaoh/utilities/AnimeGif.java
+++ src/main/java/com/gnaoh/utilities/AnimeGif.java
@@ -3,5 +3,6 @@ package com.gnaoh.utilities;
 public class AnimeGif {
     public static final String error = "https://media.giphy.com/media/AHj0lQstZ9I9W/giphy.gif",
                             smile = "https://media.giphy.com/media/wkW0maGDN1eSc/giphy.gif",
-                            shy = "https://media.giphy.com/media/UrPxdGW62TDtS/giphy.gif";
+                            shy = "https://i.pinimg.com/originals/71/de/78/71de7826ad02a908a1c3e572f50e6901.gif",
+                            shine = "https://c.tenor.com/3cnefMD2rTUAAAAM/shine-hanamaru-kindergarten.gif";
 }
