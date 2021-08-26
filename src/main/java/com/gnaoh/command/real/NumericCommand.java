4408365b0a89d21a83adfee9d154eb79edf4f8b9
hoang1007 <20020414@vnu.edu.vn>
2021-08-26 17:30:44 +0700

    fix parse long integer in power command

--- src/main/java/com/gnaoh/command/real/NumericCommand.java
+++ src/main/java/com/gnaoh/command/real/NumericCommand.java
@@ -34,11 +34,12 @@ public class NumericCommand extends Command {
     public void pow(String param1, String param2) {
         try {
             Bignum bn1 = Bignum.Parse(param1);
+            Bignum.Parse(param2);
             long k = Long.parseLong(param2);
             String resStr = Calculator.power(bn1, k).toString();
 
             if (resStr.length() < 50) event.getChannel().sendMessage(resStr).queue();
-            else event.getChannel().sendMessage(resStr)
+            else event.getChannel().sendMessage(resStr + "\nVai lon kho day!")
                 .embed(new EmbedBuilder().setImage(AnimeGifs.hard.one).build()).queue();
         } catch (IllegalArgumentException e) {
             event.getChannel().sendMessage(e.getMessage())
