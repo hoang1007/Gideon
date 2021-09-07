9112950c60a6eb2e5473523af026f5fe8ab75a11
hoang1007 <20020414@vnu.edu.vn>
2021-09-07 15:38:01 +0700

    remove unecessary var

--- src/main/java/com/gnaoh/util/lavaplayer/AudioPlayerSendHandler.java
+++ src/main/java/com/gnaoh/util/lavaplayer/AudioPlayerSendHandler.java
@@ -3,30 +3,27 @@ package com.gnaoh.util.lavaplayer;
 import java.nio.ByteBuffer;
 
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
-import com.sedmelluq.discord.lavaplayer.track.playback.MutableAudioFrame;
+import com.sedmelluq.discord.lavaplayer.track.playback.AudioFrame;
 
 import net.dv8tion.jda.api.audio.AudioSendHandler;
 
 public class AudioPlayerSendHandler implements AudioSendHandler {
     private final AudioPlayer audioPlayer;
-    private final ByteBuffer buffer;
-    private final MutableAudioFrame frame;
+    private AudioFrame lastFrame;
 
     public AudioPlayerSendHandler(AudioPlayer audioPlayer) {
         this.audioPlayer = audioPlayer;
-        this.buffer = ByteBuffer.allocate(1024);
-        this.frame = new MutableAudioFrame();
-        this.frame.setBuffer(buffer);
     }
 
     @Override
     public boolean canProvide() {
-        return audioPlayer.provide(frame);
+        lastFrame = audioPlayer.provide();
+        return lastFrame != null;
     }
 
     @Override
     public ByteBuffer provide20MsAudio() {
-        return buffer.flip();
+        return ByteBuffer.wrap(lastFrame.getData());
     }
     
     @Override
